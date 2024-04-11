eeb60a52e628172aa77a2b4916ba4ac9a9927a60
eduardormalves <139605693+eduardormalves@users.noreply.github.com>
2024-04-11 00:30:41 -0300

    script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
 window.sr = ScrollReveal({reset: true});
-
 sr.reveal('.trabalhos', {duration: 1500});
+if (innerWidth >= 903) {
+    sr = ScrollReveal({reset: true});
+    sr.reveal('.trabalhos', {duration: 1500});
+}
